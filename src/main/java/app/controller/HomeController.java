76292ab97072c5372581e6ee3f065a9e5ee0471a
Alex Balak <alex.balak@outlook.com>
2025-06-12 15:53:35 +0200

    Added Name to homepage

--- src/main/java/app/controller/HomeController.java
+++ src/main/java/app/controller/HomeController.java
@@ -3,11 +3,13 @@ package app.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class HomeController {
 
     @GetMapping("/")
-    String home() {
-        return "Home";
+    String home(Principal principal) {
+        return "Welcome Home " + principal.getName();
     }
 }
